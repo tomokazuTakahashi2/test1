a938c0a44b1ee69c56d972401720a01824ed77a7
tomokazu.takahashi2 <takahashitomokazu4@gmail.com>
2019-08-03 19:36:50 +0900

    Initial Commit

---
+++ 辞書アプリ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  辞書アプリ
+//
+//  Created by ラファエル on 2019/08/03.
+//  Copyright © 2019 takahashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
