42da93bd4e54e3e50f8c342ec651e7167c13b80a
tomokazu.takahashi2 <takahashitomokazu4@gmail.com>
2019-08-05 00:40:38 +0900

    コメントを追加

--- jisyoapp/jisyoapp/ViewController.swift
+++ jisyoapp/jisyoapp/ViewController.swift
@@ -1,29 +1,18 @@
-//
-//  ViewController.swift
-//  辞書アプリ
-//
-//  Created by ラファエル on 2019/08/03.
-//  Copyright © 2019 takahashi. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-    
+
     @IBOutlet weak var textField: UITextField!
- 
     @IBOutlet weak var label: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
 
     @IBAction func button(_ sender: Any) {
+//dictionaryにはりんご、バナナ、ぶどう、もも、なしが登録されています
         let dictionary: [String : String] = [
             "apple":"りんご",
             "banana":"バナナ",
@@ -31,13 +20,17 @@ class ViewController: UIViewController {
             "peach":"もも",
             "pear":"なし"
         ]
+        //textFieldに入力したものがinputTextで
         let inputText : String = textField.text!
-        
+        //もし、inputTexがdictionaryの登録単語であれば、それはwordとし、labelに表示する
+        //※オプショナルバインディング
         if let word : String = dictionary[inputText]{
             label.text = word
+        //そうでない場合は「登録されていません」とlabelに表示される
         }else{
             label.text = "登録されていません"
-        }
     }
+    
 }
 
+}
